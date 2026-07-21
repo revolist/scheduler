bb7443b35b45ee8e4ab5a9a0ef3cd7ab89f8a908
maks <kudrytskimaksim@gmail.com>
2026-07-21 13:14:41 +0100

    feat/improved demos

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import '../../demo-host.css';
+import '@revolist/revogrid-pro/dist/revogrid-pro.css';
+import '@revolist/revogrid-enterprise/dist/revogrid-enterprise.css';
+import { load } from './index';
+
+load('#app');
