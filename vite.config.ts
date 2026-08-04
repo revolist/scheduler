b313c4b693b3ee569e281c539ab76dbf85c2aa78
maks <kudrytskimaksim@gmail.com>
2026-08-04 15:39:25 +0100

    fix/introduced new demos

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite';
 import angular from '@analogjs/vite-plugin-angular';
 import react from '@vitejs/plugin-react';
 import vue from '@vitejs/plugin-vue';
+import { trialCssAliases } from '../vite.trial-aliases';
 
 export default defineConfig(({ mode }) => ({
-  resolve: mode === 'angular' ? { mainFields: ['module'] } : undefined,
+  resolve: {
+    alias: trialCssAliases,
+    ...(mode === 'angular' ? { mainFields: ['module'] } : {}),
+  },
   plugins: [
     ...(mode === 'angular' ? [angular()] : []),
     react(),
